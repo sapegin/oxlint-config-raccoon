9564e885bdbd326272947b92afc4990e7d61ab45
Artem Sapegin <artem@sapegin.me>
2026-06-22 10:26:40 +0200

    fix: Disable trailing commas in JSONC to make them more compatible

--- src/oxfmt.ts
+++ src/oxfmt.ts
@@ -15,11 +15,19 @@ const config: OxfmtConfig = {
   sortTailwindcss: {},
   overrides: [
     {
+      // More readable options for Markdown files
       files: ['*.md'],
       options: {
         arrowParens: 'avoid',
         printWidth: 70,
         proseWrap: 'never',
+        trailingComma: 'none',
+      },
+    },
+    {
+      // Disable trailing commas in JSONC to make them more compatible
+      files: ['*.jsonc'],
+      options: {
         trailingComma: 'none',
       },
     },
